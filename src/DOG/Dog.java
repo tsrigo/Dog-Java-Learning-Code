98d1d916d0c50f13ffe3b4db9c26ed5de85179b6
tsrigo <717660037@qq.com>
2022-06-26 17:15:31 +0800

    Replace old method(makeNoise) with non-static (instance) method to verify different dogs. Show one way to init d(instance of Dog)

--- src/DOG/Dog.java
+++ src/DOG/Dog.java
@@ -1,8 +1,18 @@
 package DOG;
 
 public class Dog {
-    public static void makeNoise(){
+    public int weight;
+
+    public void makeNoise(){
+        if (weight < 10){
+            System.out.println("yipyip ~~ ");
+        } else if (weight < 30 ){
+            System.out.println("bark ! bark !");
+        } else {
+            System.out.println("Woooooooooooooooof !!!!!!! ");
+        }
+    }
+    public static void Old_makeNoise(){
         System.out.println("Bark !!!");
     }
 }
-
--- src/DOG/DogLauncher.java
+++ src/DOG/DogLauncher.java
@@ -2,6 +2,9 @@ package DOG;
 
 public class DogLauncher {
     public static void main(String[] args){
-        Dog.makeNoise();
+        Dog d;
+        d = new Dog();
+        d.weight = 20;
+        d.makeNoise();
     }
 }
